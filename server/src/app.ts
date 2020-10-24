635647ac816d15ee95bb02372d4ffdcd0d2d2011
Mauricio Marin <marin@Mauricios-MacBook-Pro.local>
2020-10-23 23:12:55 -0700

    Add initial routes set up

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,11 +1,10 @@
-import express, {Request, Response, Application} from 'express'
+import express, { Application } from 'express'
+import movies from './routes/movies';
 
 const app: Application = express();
 
-app.get('/', (req: Request, res: Response) => {
-  res.json({msg: "Hello world!"})
-})
+app.use('/movies', movies);
 
-const PORT: string | number = process.env.PRODUCTION ||5000;
+const PORT: string | number = process.env.PORT ||5000;
 
 app.listen(5000, () => console.log(`Server running on port: ${PORT}...`))
